2388dbde59262159582dbb2a63deaa926d5ffd5e
Your Name <natownley@gmail.com>
2019-07-14 14:01:21 -0500

    code for random selection and length of gamme in javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var words = ["attractive", "dusty", "kind", "quirky", "dumb","intelligent", "humorous", "caring", "loving", "gregarious", "exhuberant", "outgoing", "loud","demonstrative", "renaissance","assertive", "fashionsita", "chubby", "leader", "aries","coppertone" ];
+var wordPick = words[Math.floor(Math.random () *words.length)];
+var answerLength = [];
+for (var i=0; i < wordPick.length; i++) {answerLength [i]="_";
+}
